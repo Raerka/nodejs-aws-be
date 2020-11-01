8786d667a7cff993f457fbc33a68cb9e7b6f0cde
Andrei Kasmykou <andrei_kasmykou@epam.com>
2020-11-01 23:01:50 +0300

    task 3

---
+++ product-service/serverless.ts
@@ -0,0 +1,55 @@
+import { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'service-product',
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    getProductsList: {
+      handler: 'handler.getProductsList',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'products',
+            cors: true,
+          },
+        },
+      ],
+    },
+    getProductsById: {
+      handler: 'handler.getProductsById',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'products/{productId}',
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
---
+++ product-service/src/handlers/getProductsList.ts
@@ -0,0 +1,23 @@
+import 'source-map-support/register';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { getAllProducts } from '../service/productService';
+
+export const getProductsList: APIGatewayProxyHandler = async () => {
+  try {
+    const products = await getAllProducts();
+
+    return {
+      statusCode: 200,
+      body: products,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  } catch (err) {
+    return {
+      statusCode: err,
+      body: 'Internal Server Error',
+    };
+  }
+};
