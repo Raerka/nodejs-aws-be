6ef02c802898477a6f7f2e9d2b7dd2a8d31c4dbb
Andrei Kasmykou <andrei_kasmykou@epam.com>
2020-11-03 10:25:10 +0300

    remove comments

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,12 +1,2 @@
 export { getProductsList } from './src/handlers';
 export { getProductsById } from './src/handlers';
-
-// export const hello: APIGatewayProxyHandler = async (event, _context) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-//       input: event,
-//     }, null, 2),
-//   };
-// }
